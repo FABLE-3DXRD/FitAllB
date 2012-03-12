e5714c7899ee2f9937f7950533f489dae4526362
jetteodder <jetteodder@users.sourceforge.net>
2012-03-12 15:00:11 +0000

    bugfix in setup.py

--- setup.py
+++ setup.py
@@ -7,9 +7,9 @@ setup(
   version='1.1.0',
   description='Fitting routines for global parameters (fitgloball), global parameters for each grain (fitglobalgrain) and grain cms, orientations and strain (fitallb)',
   license='GPL', maintainer='Jette Oddershede',
-  maintainer_email='jette.oddershede@risoe.dk',
+  maintainer_email='jeto@fysik.dtu.dk',
   url='http://fable.wiki.sourceforge.net',
   packages=["FitAllB"],
   package_dir={"FitAllB":"FitAllB"},
-  scripts=["scripts/fitallb.py","scripts/fitallb_1D.py","scripts/fitgloball.py","scripts/fitglobalgrain.py","scripts/fitgloball_multidet.py"]
+  scripts=["scripts/fitallb.py","scripts/fitgloball.py","scripts/fitglobalgrain.py","scripts/fitgloball_multidet.py"]
 )
